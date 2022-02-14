6265b2f01b3f7c8520f22e7eb9da75e2235d67aa
Conor ladrigan <conorladrigan95@gmail.com>
2022-02-14 10:16:41 +0000

    created product model

---
+++ src/models/Product.ts
@@ -0,0 +1,87 @@
+import mongoose, { Schema, model } from 'mongoose';
+import { User } from './User';
+
+export interface Product {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
+  company: string;
+  colors: string[];
+  featured: boolean;
+  inventory: number;
+  freeShipping: boolean;
+  averageRating: number;
+  rating: number;
+  user: User;
+}
+
+const ProductSchema = new Schema<Product>(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide product name'],
+      maxLength: [100, 'Name cannot be greater than 100 characters'],
+    },
+    price: {
+      type: Number,
+      default: 0,
+      required: [true, 'Please provide product price'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide product description'],
+      maxLength: [1000, 'Description cannot be more than 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/example.jpeg',
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['office', 'kitchen', 'bedroom'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Please provide product company'],
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productModel = model<Product>('Product', ProductSchema);
+
+export default productModel;
